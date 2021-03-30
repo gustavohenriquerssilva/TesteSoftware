75ecd56453971ea4ff4b65233cf7c29e3021c4f5
gustavohenriquerssilva <gustavohenriquerssilva@gmail.com>
2021-03-29 21:18:51 -0300

    Adiciona exeçao

--- sum.js
+++ sum.js
@@ -1,4 +1,9 @@
 function sum(a, b) {
+  if (a == null || b== null) {
+    throw new TypeError("Parâmetro inválido. O valor é nulo.");
+  }
+
   return a + b;
 }
+
 module.exports = sum;
--- sum.test.js
+++ sum.test.js
@@ -3,3 +3,7 @@ const sum = require('./sum');
 test('Adicição de 1 + 2 é igual a 3', () => {
   expect(sum(1, 2)).toBe(3);
 });
+
+test("argumento null/undefined", () => {
+  expect(() => { sum(); }).toThrow(TypeError);
+});
